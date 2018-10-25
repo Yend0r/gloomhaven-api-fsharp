b0ce23d4dc383e552071a5b11be20ce3e67533a6
Unknown <rod.buchan@gmail.com>
2018-10-25 18:22:42 +1100

    Made the authentication a bit simpler
    I think it could be improved more.

--- src/GloomChars.Authentication/AuthenticationService.fs
+++ src/GloomChars.Authentication/AuthenticationService.fs
@@ -5,6 +5,12 @@ open System.Text
 open GloomChars.Common
 open FSharpPlus
 
+type AuthFailure =
+    | EmailNotInSystem
+    | PasswordMismatch of PreAuthUser
+    | IsLockedOut of string
+    | ErrorSavingToken
+
 [<RequireQualifiedAccess>]
 module AuthenticationService =
 
@@ -22,10 +28,7 @@ module AuthenticationService =
         }
         |> dbUpdateLoginStatus
 
-    let private clearLockout updateStatus userId = 
-        updateStatus (userId, 0, false)
-
-    let private updateLockoutStatus updateStatus (attemptsBeforeLockout : int) user = 
+    let private logAuthFailure updateStatus (attemptsBeforeLockout : int) user = 
         //Update details with the login attempt
         let attemptNumber = user.LoginAttemptNumber + 1
         let isLockedOut = (attemptNumber > attemptsBeforeLockout)
@@ -43,50 +46,39 @@ module AuthenticationService =
                 //let dateLockedOut = dateLockedOut.AddMinutes(float lockoutDuration)
                 //let msg = sprintf "Locked out: %i mins (%A - %A)" lockoutDuration dateLockedOut DateTime.UtcNow
                 let msg = sprintf "Account is locked out. Please wait %i mins or contact an administrator." lockoutDuration
-                Error msg
+                Error (IsLockedOut msg)
 
         | NotLockedOut -> 
             Ok user
 
-    let private checkPassword (onInvalidPwd : PreAuthUser -> unit) password (user : PreAuthUser) = 
+    let private verifyPassword password (user : PreAuthUser) = 
         //If we got a user then always do the password check to hamper time based attacks
-        let passwordVerified = PasswordHasher.verifyHashedPassword(user.Email, user.PasswordHash, password)
+        let passwordVerified = 
+            PasswordHasher.verifyHashedPassword(user.Email, user.PasswordHash, password)
 
         match passwordVerified with 
-        | true -> 
-            Ok user
-        | false -> 
-            //Increment the number of login attempts... may result in user being locked out 
-            onInvalidPwd(user) 
-            Error "Invalid email/password."
+        | true -> Ok user
+        | false -> Error (PasswordMismatch user)
+
+    let private createAccessToken() = 
+        AccessToken(string (Guid.NewGuid())) 
 
     let private makeNewLogin userId tokenDuration = 
         {
             UserId = userId
-            AccessToken = AccessToken(string (Guid.NewGuid())) 
+            AccessToken = createAccessToken() 
             AccessTokenExpiresAt = DateTime.UtcNow.AddMinutes(float tokenDuration) 
         }
 
     let private createLogin
         (config : AuthenticationConfig) 
-        (dbInsertNewLogin : NewLogin -> Result<AccessToken, string>)
-        (onSuccessfulLogin : int -> unit)
+        (dbInsertNewLogin : NewLogin -> Result<NewLogin, string>)
         (user : PreAuthUser) = 
 
-        let token = AccessToken(string (Guid.NewGuid()))
-        let tokenExpiry = DateTime.UtcNow.AddMinutes(float config.AccessTokenDurationInMins) 
-
         //Log them in
-        (user.Id, config.AccessTokenDurationInMins)
-        ||> makeNewLogin 
+        makeNewLogin user.Id config.AccessTokenDurationInMins
         |> dbInsertNewLogin 
-        |> function 
-        | Ok token -> 
-            //Update user details - reset login attempts to zero
-            onSuccessfulLogin(user.Id)
-            Ok token
-        | Error err -> 
-            Error err
+        |> Result.mapError (fun _ -> ErrorSavingToken)
 
     let private hashFakePassword() = 
         let fakePwd = Guid.NewGuid().ToString() |> Encoding.UTF8.GetBytes |> Convert.ToBase64String
@@ -98,32 +90,53 @@ module AuthenticationService =
             //No such user... do a fake password check (to take the same time as a real email) 
             //so attackers cannot tell that the email doesn't exist. 
             hashFakePassword()
-            Error "Invalid email/password."
+            Error EmailNotInSystem
         | Some user ->
             Ok user
+
+    let private onSuccess updateStatus (newLogin : NewLogin) = 
+        //Clear any failed lockout attempts
+        updateStatus (newLogin.UserId, 0, false) 
+        newLogin.AccessToken
+
+    let private onFailure updateStatus config authError = 
+        match authError with
+        | EmailNotInSystem -> 
+            "Invalid email/password."
+        | IsLockedOut msg -> 
+            msg
+        | PasswordMismatch user ->
+            logAuthFailure updateStatus config.LoginAttemptsBeforeLockout user
+            "Invalid email/password."
+        | ErrorSavingToken -> 
+            //Caused by token collision? If so then making the user try again should fix it
+            "Invalid email/password." 
 
     let authenticate 
         (config : AuthenticationConfig)
         (dbGetUserForAuth : string -> PreAuthUser option)
         (dbUpdateLoginStatus : LoginStatusUpdate -> unit)
-        (dbInsertNewLogin : NewLogin -> Result<AccessToken, string>)
+        (dbInsertNewLogin : NewLogin -> Result<NewLogin, string>)
         (email : string)
         (password : string) = 
 
         //Partially apply some fns to make the final logic clearer
         let getUser = dbGetUserForAuth |> getUserByEmail 
         let updateStatus = dbUpdateLoginStatus |> updateLoginStatus  
-        let verifyPassword = (updateStatus, config.LoginAttemptsBeforeLockout) ||> updateLockoutStatus |> checkPassword 
         let checkIfLockedOut = config.LockoutDurationInMins |> checkLockout 
-        let onSuccessfulLogin = updateStatus |> clearLockout 
-        let createAccessToken = (config, dbInsertNewLogin, onSuccessfulLogin) |||> createLogin 
+        let createAccessToken = (config, dbInsertNewLogin) ||> createLogin 
 
         getUser email
         >>= verifyPassword password 
         >>= checkIfLockedOut
         >>= createAccessToken
-
-    let getAuthenticatedUser (dbGetAuthenticatedUser : AccessToken -> AuthenticatedUser option) accessToken = 
+        |> map (onSuccess updateStatus)
+        |> Result.mapError (onFailure updateStatus config)
+ 
+    let getAuthenticatedUser 
+        (dbGetAuthenticatedUser : AccessToken -> AuthenticatedUser option) 
+        accessToken = 
+        
         match dbGetAuthenticatedUser accessToken with 
         | None -> Error "Invalid access token."
         | Some user -> Ok user
